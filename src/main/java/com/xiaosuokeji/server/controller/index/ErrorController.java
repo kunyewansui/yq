8cdadeb7c3a0ed38fe3960c00c49d5ac0667d928
xuxiaowei <xxw@xiaosuokeji.com>
2017-11-03 15:25:33 +0800

    目录结构更新

--- src/main/java/com/xiaosuokeji/server/controller/index/ErrorController.java
+++ src/main/java/com/xiaosuokeji/server/controller/index/ErrorController.java
@@ -1,4 +1,4 @@
-package com.xiaosuokeji.server.controller.system;
+package com.xiaosuokeji.server.controller.index;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
