f21692dca70548a6722862b58a69f792c829d65d
xuxiaowei <xxw@xiaosuokeji.com>
2017-11-09 22:07:17 +0800

    系统日志功能模块

--- src/main/java/com/xiaosuokeji/server/manager/security/AuthenticationSuccessHandlerImpl.java
+++ src/main/java/com/xiaosuokeji/server/manager/security/AuthenticationSuccessHandlerImpl.java
@@ -1,6 +1,11 @@
 package com.xiaosuokeji.server.manager.security;
 
 import com.xiaosuokeji.framework.constant.XSStatusConstant;
+import com.xiaosuokeji.framework.util.XSIpUtil;
+import com.xiaosuokeji.server.model.security.SecStaff;
+import com.xiaosuokeji.server.model.security.SecStaffLog;
+import com.xiaosuokeji.server.service.intf.security.SecStaffLogService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Service;
@@ -9,6 +14,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 /**
  * 系统用户登录成功处理器
@@ -17,6 +23,9 @@ import java.io.IOException;
 @Service("authenticationSuccessHandler")
 public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHandler {
 
+    @Autowired
+    private SecStaffLogService secStaffLogService;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) throws IOException, ServletException {
@@ -24,5 +33,15 @@ public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHa
         response.setCharacterEncoding("UTF-8");
         response.setContentType("application/json;charset=utf-8");
         response.getWriter().write(successMsg);
+        //记录成功登录日志
+        SecStaffLog latest = new SecStaffLog();
+        latest.setStaff((SecStaff) authentication.getPrincipal());
+        latest.setIp(XSIpUtil.getRequestIp(request));
+        latest.setTime(new Date());
+        latest.setOperation("登录系统");
+        latest.setStatus(1);
+        latest.setParameters("");
+        latest.setResult("");
+        secStaffLogService.save(latest);
     }
 }
--- src/main/java/com/xiaosuokeji/server/manager/security/DetailsService.java
+++ src/main/java/com/xiaosuokeji/server/manager/security/DetailsService.java
@@ -1,12 +1,9 @@
 package com.xiaosuokeji.server.manager.security;
 
 import com.xiaosuokeji.framework.constant.XSStatusConstant;
-import com.xiaosuokeji.framework.util.XSIpUtil;
 import com.xiaosuokeji.server.constant.security.SecStaffConsts;
 import com.xiaosuokeji.server.model.security.SecRole;
 import com.xiaosuokeji.server.model.security.SecStaff;
-import com.xiaosuokeji.server.model.security.SecStaffLog;
-import com.xiaosuokeji.server.service.intf.security.SecStaffLogService;
 import com.xiaosuokeji.server.service.intf.security.SecStaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.DisabledException;
@@ -16,13 +13,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 
 /**
  * 系统用户登录Service
@@ -33,9 +26,6 @@ public class DetailsService implements UserDetailsService {
 
     @Autowired
     private SecStaffService secStaffService;
-
-    @Autowired
-    private SecStaffLogService secStaffLogService;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -56,16 +46,6 @@ public class DetailsService implements UserDetailsService {
             authorities.add(new SimpleGrantedAuthority("ROLE_" + String.valueOf(role.getId())));
         }
         staff.setAuthorityList(authorities);
-        //记录登录日志
-        SecStaffLog latest = new SecStaffLog();
-        latest.setStaff(staff);
-        latest.setIp("");
-        latest.setTime(new Date());
-        latest.setOperation("登录系统");
-        latest.setStatus(1);
-        latest.setParameters("");
-        latest.setResult("");
-        secStaffLogService.save(latest);
         return staff;
     }
 }
